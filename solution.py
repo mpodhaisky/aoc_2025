b96738edf645f9a7cf7bac1a52bc8880e19fecfe
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2025-11-30 18:04:10 +0100

    setup files

---
+++ solution.py
@@ -0,0 +1,20 @@
+from collections import Counter, defaultdict
+import hashlib
+import re
+import math
+from heapq import heappop, heappush, heapify
+
+adj4 = [(-1,0),(0,1),(1,0),(0,-1)]
+adj8 = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+
+# trans={"U":(-1,0),"L":(0,-1), "D":(1,0),"R":(0,1)}
+def nums(line):
+    return list(map(int,re.findall(r'-?\d+', line)))
+
+def solve(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = open(0).read().rstrip()
+    solve(data)
